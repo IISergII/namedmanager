046cb675c0747da5e7eeff2a5de6eacd2253d1ba
IISergII <54176521+IISergII@users.noreply.github.com>
2020-06-18 19:21:20 +0400

    Update to php 7.4

--- htdocs/domains/delete-process.php
+++ htdocs/domains/delete-process.php
@@ -1,101 +1,74 @@
 <?php
+
 /*
-	domains/delete-process.php
+  domains/delete-process.php
 
-	access:
-		namedadmins
+  access:
+  namedadmins
 
-	Deletes an unwanted domain.
-*/
-
+  Deletes an unwanted domain.
+ */
 
 // includes
-require("../include/config.php");
-require("../include/amberphplib/main.php");
-require("../include/application/main.php");
+require_once "../include/config.php";
+require_once "../include/amberphplib/main.php";
+require_once "../include/application/main.php";
 
+if (user_permissions_get('namedadmins')) {
+    /*
+      Form Input
+     */
 
-if (user_permissions_get('namedadmins'))
-{
-	/*
-		Form Input
-	*/
+    $obj_domain = New domain;
+    $obj_domain->id = security_form_input_predefined("int", "id_domain", 1, "");
 
-	$obj_domain		= New domain;
-	$obj_domain->id		= security_form_input_predefined("int", "id_domain", 1, "");
+    // for error return if needed
+    security_form_input_predefined("any", "domain_name", 0, "");
+    security_form_input_predefined("any", "domain_description", 0, "");
 
+    // confirm deletion
+    security_form_input_predefined("any", "delete_confirm", 1, "You must confirm the deletion");
 
-	// for error return if needed
-	@security_form_input_predefined("any", "domain_name", 0, "");
-	@security_form_input_predefined("any", "domain_description", 0, "");
+    /*
+      Verify Data
+     */
 
-	// confirm deletion
-	@security_form_input_predefined("any", "delete_confirm", 1, "You must confirm the deletion");
+    // verify the selected domain exists
+    if (!$obj_domain->verify_id()) {
+        log_write("error", "process", "The domain you have attempted to delete - " . $obj_domain->id . " - does not exist in this system.");
+    }
 
+    /*
+      Process Data
+     */
 
+    if (error_check()) {
+        $_SESSION["error"]["form"]["domain_delete"] = "failed";
+        header("Location: ../index.php?page=domains/delete.php&id=" . $obj_domain->id . "");
 
+        exit(0);
+    } else {
+        // clear error data
+        error_clear();
 
-	/*
-		Verify Data
-	*/
+        /*
+          Delete domain
+         */
 
+        $obj_domain->load_data();
+        $obj_domain->action_delete();
 
-	// verify the selected domain exists
-	if (!$obj_domain->verify_id())
-	{
-		log_write("error", "process", "The domain you have attempted to delete - ". $obj_domain->id ." - does not exist in this system.");
-	}
+        /*
+          Return
+         */
 
-
-
-
-	/*
-		Process Data
-	*/
-
-	if (error_check())
-	{
-		$_SESSION["error"]["form"]["domain_delete"]	= "failed";
-		header("Location: ../index.php?page=domains/delete.php&id=". $obj_domain->id ."");
-
-		exit(0);
-	}
-	else
-	{
-		// clear error data
-		error_clear();
-
-
-
-		/*
-			Delete domain
-		*/
-
-		$obj_domain->load_data();
-
-		$obj_domain->action_delete();
-
-
-
-		/*
-			Return
-		*/
-
-		header("Location: ../index.php?page=domains/domains.php");
-		exit(0);
-
-
-	} // if valid data input
-	
-	
+        header("Location: ../index.php?page=domains/domains.php");
+        exit(0);
+    } // if valid data input
 } // end of "is user logged in?"
-else
-{
-	// user does not have permissions to access this page.
-	error_render_noperms();
-	header("Location: ../index.php?page=message.php");
-	exit(0);
+else {
+    // user does not have permissions to access this page.
+    error_render_noperms();
+    header("Location: ../index.php?page=message.php");
+    exit(0);
 }
-
-
-?>
--- htdocs/domains/domains.php
+++ htdocs/domains/domains.php
@@ -1,170 +1,148 @@
 <?php
+
 /*
-	domains/domains.php
+  domains/domains.php
 
-	access:
-		namedadmins
+  access:
+  namedadmins
 
-	Interface to view and manage domains.
-*/
+  Interface to view and manage domains.
+ */
 
+class page_output {
 
-class page_output
-{
-	var $obj_table;
-	var $obj_form;
-	var $invalid_filter;
+    public $obj_table;
+    public $obj_form;
+    public $invalid_filter;
 
-		function page_output()
-	{
-		// include custom scripts and/or logic
-		$this->requires["javascript"][]	= "include/javascript/filter_domains.js";
+    public function __construct() {
+        // include custom scripts and/or logic
+        $this->requires["javascript"][] = "include/javascript/filter_domains.js";
 
-		// initialize filter
-		$this->invalid_filter=false;
-	}
+        // initialize filter
+        $this->invalid_filter = false;
+    }
 
-	function check_permissions()
-	{
-		return user_permissions_get("namedadmins");
-	}
+    public function check_permissions() {
+        return user_permissions_get("namedadmins");
+    }
 
-	function check_requirements()
-	{
-		// nothing todo
-		return 1;
-	}
+    public function check_requirements() {
+        // nothing todo
+        return 1;
+    }
 
+    public function execute() {
+        // Define form structure for the filter
+        $this->obj_form = New form_input;
+        $this->obj_form->formname = "filter_domains";
+        $this->obj_form->language = $_SESSION["user"]["lang"];
+        $this->obj_form->action = "/";
+        $this->obj_form->method = "get";
 
-	function execute()
-	{
-		// Define form structure for the filter
-		$this->obj_form			= New form_input;
-		$this->obj_form->formname	= "filter_domains";
-		$this->obj_form->language	= $_SESSION["user"]["lang"];
+        // filter pattern
+        $filter = NULL;
+        $filter["fieldname"] = "domain_name";
+        $filter["type"] = "input";
+        $filter["sql"] = "domain_name LIKE '%value%'";
+        $filter["defaultvalue"] = security_script_input("/^[A-Za-z0-9\.\-]*$/", $_GET["domain_name"]);
+        if ($filter["defaultvalue"] == "error") {
+            $this->invalid_filter = true;
+            $filter["defaultvalue"] = "";
+        }
+        $this->obj_form->add_input($filter);
 
-		$this->obj_form->action		= "/";
-		$this->obj_form->method		= "get";
+        // submit button
+        $structure = NULL;
+        $structure["fieldname"] = "submit";
+        $structure["type"] = "submit";
+        $structure["defaultvalue"] = "Apply Filter";
+        $this->obj_form->add_input($structure);
 
+        // establish a new table object
+        $this->obj_table = New table;
+        $this->obj_table->language = $_SESSION["user"]["lang"];
+        $this->obj_table->tablename = "name_servers";
 
-		// filter pattern
-		$filter = NULL;
-		$filter["fieldname"] 		= "domain_name";
-		$filter["type"]				= "input";
-		$filter["sql"]				= "domain_name LIKE '%value%'";
-		$filter["defaultvalue"]		= security_script_input("/^[A-Za-z0-9\.\-]*$/", $_GET["domain_name"]);
-		if ($filter["defaultvalue"] == "error")
-		{
-			$this->invalid_filter = true;
-			$filter["defaultvalue"] = "";
-		}
-		$this->obj_form->add_input($filter);
+        // define all the columns and structure
+        $this->obj_table->add_column("standard", "domain_name", "domain_name");
+        $this->obj_table->add_column("standard", "domain_serial", "soa_serial");
+        $this->obj_table->add_column("standard", "domain_description", "domain_description");
 
-		// submit button
-		$structure = NULL;
-		$structure["fieldname"] 	= "submit";
-		$structure["type"]			= "submit";
-		$structure["defaultvalue"]	= "Apply Filter";
-		$this->obj_form->add_input($structure);
+        // defaults
+        $this->obj_table->columns = array("domain_name", "domain_serial", "domain_description");
 
+        // TODO: we should stop querying directly and use the domains class logic, this would also provide support for multiple backends.
+        // use seporate zone database
+        //$this->obj_table->sql_obj->session_init("mysql", $GLOBALS["config"]["ZONE_DB_HOST"], $GLOBALS["config"]["ZONE_DB_NAME"], $GLOBALS["config"]["ZONE_DB_USERNAME"], $GLOBALS["config"]["ZONE_DB_PASSWORD"]);
+        // fetch all the domains
+        $this->obj_table->sql_obj->prepare_sql_settable("dns_domains");
+        $this->obj_table->sql_obj->prepare_sql_addfield("id", "");
+        $this->obj_table->add_filter($filter);
+        $this->obj_table->sql_obj->prepare_sql_addorderby("REVERSE(domain_name) LIKE 'apra%'");
+        $this->obj_table->sql_obj->prepare_sql_addorderby("domain_description");
+        $this->obj_table->sql_obj->prepare_sql_addorderby("domain_name");
 
-		// establish a new table object
-		$this->obj_table = New table;
+        // load data
+        $this->obj_table->generate_sql();
+        $this->obj_table->load_data_sql();
+    }
 
-		$this->obj_table->language	= $_SESSION["user"]["lang"];
-		$this->obj_table->tablename	= "name_servers";
+    public function render_html() {
+        // title + summary
+        print "<h3>DOMAINS</h3>";
+        print "<p>List of domains managed by this server:</p>";
 
-		// define all the columns and structure
-		$this->obj_table->add_column("standard", "domain_name", "domain_name");
-		$this->obj_table->add_column("standard", "domain_serial", "soa_serial");
-		$this->obj_table->add_column("standard", "domain_description", "domain_description");
+        // table data
+        if ((!$this->obj_table->data_num_rows) and (!$_GET["domain_name"])) {
+            format_msgbox("important", "<p>There are currently no domain names configured.</p>");
+        } else {
+            if ($this->invalid_filter) {
+                format_msgbox("important", "<p>Invalid filter expression.</p>");
+            }
 
-		// defaults
-		$this->obj_table->columns		= array("domain_name", "domain_serial", "domain_description");
+            // details link
+            $structure = NULL;
+            $structure["id"]["column"] = "id";
+            $this->obj_table->add_link("tbl_lnk_details", "domains/view.php", $structure);
 
-		// TODO: we should stop querying directly and use the domains class logic, this would also provide support for multiple backends.
-		// use seporate zone database
-		//$this->obj_table->sql_obj->session_init("mysql", $GLOBALS["config"]["ZONE_DB_HOST"], $GLOBALS["config"]["ZONE_DB_NAME"], $GLOBALS["config"]["ZONE_DB_USERNAME"], $GLOBALS["config"]["ZONE_DB_PASSWORD"]);
+            // domain records
+            $structure = NULL;
+            $structure["id"]["column"] = "id";
+            $this->obj_table->add_link("tbl_lnk_records", "domains/records.php", $structure);
 
-		// fetch all the domains
-		$this->obj_table->sql_obj->prepare_sql_settable("dns_domains");
-		$this->obj_table->sql_obj->prepare_sql_addfield("id", "");
-		$this->obj_table->add_filter($filter);
-		$this->obj_table->sql_obj->prepare_sql_addorderby("REVERSE(domain_name) LIKE 'apra%'");
-		$this->obj_table->sql_obj->prepare_sql_addorderby("domain_description");
-		$this->obj_table->sql_obj->prepare_sql_addorderby("domain_name");
+            // delete link
+            $structure = NULL;
+            $structure["id"]["column"] = "id";
+            $this->obj_table->add_link("tbl_lnk_delete", "domains/delete.php", $structure);
 
-		// load data
-		$this->obj_table->generate_sql();
-		$this->obj_table->load_data_sql();
-	}
+            // include page name
+            $structure = NULL;
+            $structure["fieldname"] = "page";
+            $structure["type"] = "hidden";
+            $structure["defaultvalue"] = $_GET["page"];
+            $this->obj_form->add_input($structure);
 
+            // hide hidden field "page"
+            $this->obj_form->subforms["hidden"] = array("page");
 
-	function render_html()
-	{
-		// title + summary
-		print "<h3>DOMAINS</h3>";
-		print "<p>List of domains managed by this server:</p>";
+            // show input field with button
+            $this->obj_form->subforms["Filter"] = array("domain_name", "submit");
 
-		// table data
-		if ((!$this->obj_table->data_num_rows) and (!$_GET["domain_name"]))
-		{
-			format_msgbox("important", "<p>There are currently no domain names configured.</p>");
-		}
-		else {
-			if ($this->invalid_filter)
-			{
-				format_msgbox("important", "<p>Invalid filter expression.</p>");
-			}
+            // display filter form
+            $this->obj_form->render_form();
 
-			// details link
-			$structure = NULL;
-			$structure["id"]["column"]	= "id";
-			$this->obj_table->add_link("tbl_lnk_details", "domains/view.php", $structure);
+            // display the table
+            print "<div id=\"domains\">";
+            $this->obj_table->render_table_html();
+            if (!$this->obj_table->data_num_rows) {
+                format_msgbox("important", "<p>No match on domain names with current filter expression.</p>");
+            }
+            print "</div>";
+        }
 
-			// domain records
-			$structure = NULL;
-			$structure["id"]["column"]	= "id";
-			$this->obj_table->add_link("tbl_lnk_records", "domains/records.php", $structure);
-
-			// delete link
-			$structure = NULL;
-			$structure["id"]["column"]	= "id";
-			$this->obj_table->add_link("tbl_lnk_delete", "domains/delete.php", $structure);
-
-			// include page name
-			$structure = NULL;
-			$structure["fieldname"] 	= "page";
-			$structure["type"]		= "hidden";
-			$structure["defaultvalue"]	= $_GET["page"];
-			$this->obj_form->add_input($structure);
-
-			// hide hidden field "page"
-			$this->obj_form->subforms["hidden"]	= array("page");
-
-			// show input field with button
-			$this->obj_form->subforms["Filter"]	= array("domain_name","submit");
-
-			// display filter form
-			$this->obj_form->render_form();
-
-			// display the table
-			print "<div id=\"domains\">";
-			$this->obj_table->render_table_html();
-			if (!$this->obj_table->data_num_rows)
-			{
-				format_msgbox("important", "<p>No match on domain names with current filter expression.</p>");
-			}
-			print "</div>";
-
-		}
-
-		// add link
-		print "<p><a class=\"button\" href=\"index.php?page=domains/add.php\">Add New Domain</a></p>";
-
-	}
+        // add link
+        print "<p><a class=\"button\" href=\"index.php?page=domains/add.php\">Add New Domain</a></p>";
+    }
 
 }
-
-
-?>
