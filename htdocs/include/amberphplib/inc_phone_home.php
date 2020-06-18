2df18cd63b2242ddf3c8b6365524e2a1c171d29c
IISergII <54176521+IISergII@users.noreply.github.com>
2020-06-18 19:19:12 +0400

    Update to php 7.4

--- htdocs/include/amberphplib/inc_phone_home.php
+++ htdocs/include/amberphplib/inc_phone_home.php
@@ -1,231 +1,211 @@
 <?php
+
 /*
-	inc_phone_home.php
+  inc_phone_home.php
 
-	This class provides functions for reading stats from the system and submitting
-	then back to Amberstats in a "phone-home" feature.
+  This class provides functions for reading stats from the system and submitting
+  then back to Amberstats in a "phone-home" feature.
 
-	The stats returned are a small selection of general OS/platform/server details
-	and a randomly generated unique ID, used to track the upgrade patterns of users
-	over their lifespan of using the software.
+  The stats returned are a small selection of general OS/platform/server details
+  and a randomly generated unique ID, used to track the upgrade patterns of users
+  over their lifespan of using the software.
 
-	The following is a full list and example of the types of details collected:
+  The following is a full list and example of the types of details collected:
 
-	Field                 Example
-	-----                 -----
-	Application Name      Amberdms Billing System
-	Application Version   1.3.0
-	Server App            Apache/2.0.52 (CentOS)
-	Language Version      5.1.6
-	Subscription Type     opensource
-	Subscription ID       qwertyuiop234567890
+  Field                 Example
+  -----                 -----
+  Application Name      Amberdms Billing System
+  Application Version   1.3.0
+  Server App            Apache/2.0.52 (CentOS)
+  Language Version      5.1.6
+  Subscription Type     opensource
+  Subscription ID       qwertyuiop234567890
 
 
-	Typicially this class is called at login but only actually executes once a day.
-*/
+  Typicially this class is called at login but only actually executes once a day.
+ */
 
+class phone_home {
 
-class phone_home
-{
-	var $url	= "https://www.amberdms.com/api/opensource/amberdms_phone_home.php";
-	var $url_ssl	= true;	// set to true to enforce validation of SSL certificate
+    public $url = "https://www.amberdms.com/api/opensource/amberdms_phone_home.php";
+    public $url_ssl = true; // set to true to enforce validation of SSL certificate
+    public $stats; // used to hold stats about the system for submission upstream
 
-	var $stats;	// used to hold stats about the system for submission upstream
+    /*
+      check_enabled
 
+      Check that phone home is enabled.
 
-	/*
-		check_enabled
+      Returns
+      0	Disabled
+      1	Enabled
+     */
 
-		Check that phone home is enabled.
+    public function check_enabled() {
+        if ($GLOBALS["config"]["PHONE_HOME"] == "enabled" || $GLOBALS["config"]["PHONE_HOME"] == "1") {
+            // enabled
+            return 1;
+        }
 
-		Returns
-		0	Disabled
-		1	Enabled
-	*/
-	function check_enabled()
-	{
-		if ($GLOBALS["config"]["PHONE_HOME"] == "enabled" || $GLOBALS["config"]["PHONE_HOME"] == "1")
-		{
-			// enabled
-			return 1;
-		}
+        return 0;
+    }
 
-		return 0;
+// end of check_enabled
 
-	} // end of check_enabled
+    /*
+      check_phone_home_timer
 
+      Check if it's time to phone home, we only want to do this once every 12 hours at the most, otherwise
+      it's just adding unnessary load and delays on the application if we check too often.
 
+      Returns
+      0	No need to run phone home functions
+      1	Time to call in
+     */
 
-	/*
-		check_phone_home_timer
+    public function check_phone_home_timer() {
+        $time_update = $GLOBALS["config"]["PHONE_HOME_TIMER"];
+        $time_current = mktime();
 
-		Check if it's time to phone home, we only want to do this once every 12 hours at the most, otherwise
-		it's just adding unnessary load and delays on the application if we check too often.
+        if (($time_update + 43200) < $time_current) {
+            // time to phone home (been more than 12 hours since last attempt)
+            return 1;
+        }
 
-		Returns
-		0	No need to run phone home functions
-		1	Time to call in
-	*/
-	function check_phone_home_timer()
-	{
-		$time_update		= $GLOBALS["config"]["PHONE_HOME_TIMER"];
-		$time_current		= mktime();
+        return 0;
+    }
 
-		if (($time_update + 43200) < $time_current)
-		{
-			// time to phone home (been more than 12 hours since last attempt)
-			return 1;
-		}
+// end of check_phone_home_timer
 
-		return 0;
+    /*
+      stats_generate
 
-	} // end of check_phone_home_timer
+      Generates a list of different stats and stores in $this->stats;
+     */
 
+    public function stats_generate() {
+        log_write("debug", "phone_home", "Executing stats_generate()");
 
+        /*
+          Application Details
+         */
 
+        $this->stats["app_name"] = $GLOBALS["config"]["app_name"];
+        $this->stats["app_version"] = $GLOBALS["config"]["app_version"];
 
+        /*
+          Server Details
+         */
 
-	/*
-		stats_generate
+        $this->stats["server_app"] = $_SERVER["SERVER_SOFTWARE"];
+        $this->stats["server_platform"] = phpversion();
 
-		Generates a list of different stats and stores in $this->stats;
-	*/
-	function stats_generate()
-	{
-		log_write("debug", "phone_home", "Executing stats_generate()");
+        /*
+          Configuration Information
+         */
 
-		/*
-			Application Details
-		*/
+        // account information
+        $this->stats["subscription_type"] = $GLOBALS["config"]["SUBSCRIPTION_SUPPORT"];
+        $this->stats["subscription_id"] = $GLOBALS["config"]["SUBSCRIPTION_ID"];
 
-		$this->stats["app_name"] 	= $GLOBALS["config"]["app_name"];
-		$this->stats["app_version"]	= $GLOBALS["config"]["app_version"];
+        // check account ID
+        if (!$this->stats["subscription_id"]) {
+            // if no subscription id exists, then this must be running the open source version
+            // we should create an ID so we can track requests from this server, we do this by
+            // aggregating the server name and the instance name (if any) however we then hash
+            // for privacy reasons.
 
-		/*
-			Server Details
-		*/
+            $this->stats["subscription_id"] = md5($_SERVER["SERVER_NAME"] . $_SESSION["user"]["instance"]["id"] . rand(0, 1000000));
 
-		$this->stats["server_app"]	= $_SERVER["SERVER_SOFTWARE"];
-		$this->stats["server_platform"]	= phpversion();
+            $sql_obj = New sql_query;
+            $sql_obj->string = "UPDATE config SET value='" . $this->stats["subscription_id"] . "' WHERE name='SUBSCRIPTION_ID' LIMIT 1";
+            $sql_obj->execute();
 
+            log_write("debug", "phone_home", "Generated new subscription ID of " . $this->stats["subscription_id"] . "");
+        }
 
-		/*
-			Configuration Information
-		*/
+        return 1;
+    }
 
-		// account information
-		$this->stats["subscription_type"]	= $GLOBALS["config"]["SUBSCRIPTION_SUPPORT"];
-		$this->stats["subscription_id"]		= $GLOBALS["config"]["SUBSCRIPTION_ID"];
+// end of stats_generate
 
-		// check account ID
-		if (!$this->stats["subscription_id"])
-		{
-			// if no subscription id exists, then this must be running the open source version
-			// we should create an ID so we can track requests from this server, we do this by
-			// aggregating the server name and the instance name (if any) however we then hash
-			// for privacy reasons.
+    /*
+      stats_submit()
 
-			$this->stats["subscription_id"] = @md5($_SERVER["SERVER_NAME"] . $_SESSION["user"]["instance"]["id"] . rand(0,1000000));
+      Upload the stats to the Amberstats servers for user base size & tracking purposes.
 
-			$sql_obj		= New sql_query;
-			$sql_obj->string	= "UPDATE config SET value='". $this->stats["subscription_id"] ."' WHERE name='SUBSCRIPTION_ID' LIMIT 1";
-			$sql_obj->execute();
+      Returns
+      0	Failure - possible HTTP/S transport issue such as firewalled DMZ server?
+      1	Success
+     */
 
-			log_write("debug", "phone_home", "Generated new subscription ID of ". $this->stats["subscription_id"] ."");
-		}
+    public function stats_submit() {
+        log_write("debug", "phone_home", "Executing stats_submit()");
 
+        /*
+          Update Timer
+         */
 
-		return 1;
-		
-	} // end of stats_generate
+        $sql_obj = New sql_query;
+        $sql_obj->string = "UPDATE config SET value='" . mktime() . "' WHERE name='PHONE_HOME_TIMER' LIMIT 1";
+        $sql_obj->execute();
 
+        /*
+          Submit Data
+         */
 
+        foreach ($this->stats as $key => $value) {
+            $fields_string .= $key . '=' . urlencode($value) . '&';
+        }
 
-	/*
-		stats_submit()
+        rtrim($fields_string, '&');
 
-		Upload the stats to the Amberstats servers for user base size & tracking purposes.
+        // open connection
+        $ch = curl_init();
 
-		Returns
-		0	Failure - possible HTTP/S transport issue such as firewalled DMZ server?
-		1	Success
-	*/
-	function stats_submit()
-	{
-		log_write("debug", "phone_home", "Executing stats_submit()");
+        //set the url, number of POST vars, POST data
+        curl_setopt($ch, CURLOPT_URL, $this->url);
+        curl_setopt($ch, CURLOPT_POST, count($this->stats));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 2);    // wait a max of 2 seconds
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, $this->url_ssl);  // ssl validation options
 
+        if ($this->url_ssl == false) {
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); // ssl validation options
+        }
 
-		/*
-			Update Timer
-		*/
+        // execute post
+        if (!curl_exec($ch)) {
+            log_write("debug", "process", "Unable to phone home to Amberstats server, Curl error whilst POSTing data: " . curl_error($ch));
 
-		$sql_obj		= New sql_query;
-		$sql_obj->string	= "UPDATE config SET value='". mktime() ."' WHERE name='PHONE_HOME_TIMER' LIMIT 1";
-		$sql_obj->execute();
+            curl_close($ch);
+            return 0;
+        }
 
+        // check HTTP return code
+        switch ($http_return_code = curl_getinfo($ch, CURLINFO_HTTP_CODE)) {
+            case "500":
+                log_write("debug", "process", "Unexpected fault on the Amberstats server, phone home not recorded");
+                break;
 
+            case "400":
+                log_write("debug", "process", "Invalid data provided to phone home interface, information refused.");
+                break;
 
-		/*
-			Submit Data
-		*/
+            case "200":
+                log_write("debug", "process", "Server stats successfully returned to Amberstats. Thank you for submitting.");
+                break;
 
-		foreach($this->stats as $key=>$value)
-		{
-			$fields_string .= $key.'='.urlencode($value).'&';
-		}
+            default:
+                log_write("debug", "process", "Unexpected error, HTTP code $http_return_code returned!");
+                break;
+        }
 
-		rtrim($fields_string,'&');
+        curl_close($ch);
+        return 1;
+    }
 
-		// open connection
-		$ch = curl_init();
+// end of stats_submit
+}
 
-		//set the url, number of POST vars, POST data
-		curl_setopt($ch,CURLOPT_URL,$this->url);
-		curl_setopt($ch,CURLOPT_POST,count($this->stats));
-		curl_setopt($ch,CURLOPT_POSTFIELDS,$fields_string);
-		curl_setopt($ch,CURLOPT_TIMEOUT,2);				// wait a max of 2 seconds
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, $this->url_ssl); 	// ssl validation options
-
-		if ($this->url_ssl == false)
-		{
-			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); // ssl validation options
-		}
-
-		// execute post
-		if (!curl_exec($ch))
-		{	
-			log_write("debug", "process", "Unable to phone home to Amberstats server, Curl error whilst POSTing data: ". curl_error($ch));
-			
-			curl_close($ch);
-			return 0;
-		}
-
-		// check HTTP return code
-		switch ($http_return_code = curl_getinfo($ch, CURLINFO_HTTP_CODE))
-		{
-			case "500":
-				log_write("debug", "process", "Unexpected fault on the Amberstats server, phone home not recorded");
-			break;
-
-			case "400":
-				log_write("debug", "process", "Invalid data provided to phone home interface, information refused.");
-			break;
-
-			case "200":
-				log_write("debug", "process", "Server stats successfully returned to Amberstats. Thank you for submitting.");
-			break;
-
-			default:
-				log_write("debug", "process", "Unexpected error, HTTP code $http_return_code returned!");
-			break;
-		}
-		
-		curl_close($ch);
-		return 1;
-
-	} // end of stats_submit
-
-
-} // end of phone_home class
-
-?>
+// end of phone_home class
--- htdocs/include/amberphplib/main.php
+++ htdocs/include/amberphplib/main.php
@@ -1,289 +1,246 @@
 <?php
-/*
-	AMBERPHPLIB
-
-	(c) Copyright 2009 Amberdms Ltd
-
-	www.amberdms.com
-
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU Affero General Public License version 3
-	only as published by the Free Software Foundation.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU Affero General Public License for more details.
-
-	You should have received a copy of the GNU Affero General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-	If you wish to license any components of this program under a different
-	license, please contact sales@amberdms.com for commercial licencing options.
-*/
-
-
-
 
 /*
-	CORE FUNCTIONS
+  AMBERPHPLIB
 
-	These functions are required for basic operation by all major components of
-	AMBERPHPLIB, so we define them first.
-*/
-function log_debug($category, $content)
-{
-	return log_write("debug", $category, $content);
+  (c) Copyright 2009 Amberdms Ltd
+
+  www.amberdms.com
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License version 3
+  only as published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  If you wish to license any components of this program under a different
+  license, please contact sales@amberdms.com for commercial licencing options.
+ */
+
+/*
+  CORE FUNCTIONS
+
+  These functions are required for basic operation by all major components of
+  AMBERPHPLIB, so we define them first.
+ */
+
+function log_debug($category, $content) {
+    return log_write("debug", $category, $content);
 }
 
-function log_write($type, $category, $content)
-{	
-	if (!empty($_SESSION["user"]["debug"]))
-	{
-		// write log record
-		$log_record = array();
+function log_write($type, $category, $content) {
+    if (!empty($_SESSION["user"]["debug"])) {
+        // write log record
+        $log_record = [];
 
-		$log_record["type"]	= $type;
-		$log_record["category"]	= $category;
-		$log_record["content"]	= $content;
-		$log_record["memory"]	= memory_get_usage();
-	
-		// this provided PHP 4 compadiblity.
-		// TODO: when upgrading to PHP 5, replace with microtime(TRUE).
-		list($usec, $sec)		= explode(" ", microtime());
-		$log_record["time_usec"]	= $usec;
-		$log_record["time_sec"]		= $sec;
-		
-		$_SESSION["user"]["log_debug"][] = $log_record;
-		
-		// print log messages when running from CLI
-		if (!empty($_SESSION["mode"]))
-		{
-			if ($_SESSION["mode"] == "cli")
-			{
-				$content = str_replace("\n", "\\n", $content);	// fix newlines
+        $log_record["type"] = $type;
+        $log_record["category"] = $category;
+        $log_record["content"] = $content;
+        $log_record["memory"] = memory_get_usage();
 
-				print "Debug: ". sprintf("%-10.10s", $type) ." | ". sprintf("%-20.20s", $category) ." | $content\n";
-			}
-		}
-	}
+        // this provided PHP 4 compadiblity.
+        // TODO: when upgrading to PHP 5, replace with microtime(TRUE).
+        list($usec, $sec) = explode(" ", microtime());
+        $log_record["time_usec"] = $usec;
+        $log_record["time_sec"] = $sec;
 
-	// also add error messages to the error array
-	if ($type == "error")
-	{
-		$_SESSION["error"]["message"][] = $content;
-		
-		// print log messages when running from CLI
-		if (isset($_SESSION["mode"]) && $_SESSION["mode"] == "cli")
-			print date("Y-m-d H:i:s") . " Error: " . $content . "\n";
-	}
+        $_SESSION["user"]["log_debug"][] = $log_record;
 
-	// also add notification messages to the notification array
-	if ($type == "notification")
-	{
-		$_SESSION["notification"]["message"][] = $content;
-		
-		// print log messages when running from CLI
-		if (isset($_SESSION["mode"]) && ($_SESSION["mode"] == "cli"))
-			print date("Y-m-d H:i:s", $time_last) . " Notification: " . $content . "\n";
-	}
+        // print log messages when running from CLI
+        if (!empty($_SESSION["mode"])) {
+            if ($_SESSION["mode"] == "cli") {
+                $content = str_replace("\n", "\\n", $content); // fix newlines
 
+                print "Debug: " . sprintf("%-10.10s", $type) . " | " . sprintf("%-20.20s", $category) . " | $content\n";
+            }
+        }
+    }
+
+    // also add error messages to the error array
+    if ($type == "error") {
+        $_SESSION["error"]["message"][] = $content;
+
+        // print log messages when running from CLI
+        if (isset($_SESSION["mode"]) && $_SESSION["mode"] == "cli") {
+            print date("Y-m-d H:i:s") . " Error: " . $content . "\n";
+        }
+    }
+
+    // also add notification messages to the notification array
+    if ($type == "notification") {
+        $_SESSION["notification"]["message"][] = $content;
+
+        // print log messages when running from CLI
+        if (isset($_SESSION["mode"]) && ($_SESSION["mode"] == "cli")) {
+            print date("Y-m-d H:i:s", $time_last) . " Notification: " . $content . "\n";
+        }
+    }
 }
 
-
-
-
 /*
-	INCLUDE MAJOR AMBERDPHPLIB COMPONENTS
-*/
+  INCLUDE MAJOR AMBERDPHPLIB COMPONENTS
+ */
 
-@log_debug("start", "");
-@log_debug("start", "AMBERPHPLIB STARTED");
-@log_debug("start", "Debugging for: ". str_replace("&", " &", $_SERVER["REQUEST_URI"]) ."");
-@log_debug("start", "");
-
+log_debug("start", "");
+log_debug("start", "AMBERPHPLIB STARTED");
+log_debug("start", "Debugging for: " . str_replace("&", " &", $_SERVER["REQUEST_URI"]) . "");
+log_debug("start", "");
 
 // Important that we require language first, since other functions
 // require it.
-require("inc_language.php");
+require_once "inc_language.php";
 
 // DB SQL processing and execution
-require("inc_sql.php");
-require("inc_ldap.php");
+require_once "inc_sql.php";
+require_once "inc_ldap.php";
 
 // User + Security Functions
-require("inc_user.php");
-require("inc_security.php");
+require_once "inc_user.php";
+require_once "inc_security.php";
 
 // Error Handling
-require("inc_errors.php");
+require_once "inc_errors.php";
 
 // Misc Functions
-require("inc_misc.php");
+require_once "inc_misc.php";
 
 // Template processing engines
-require("inc_template_engines.php");
+require_once "inc_template_engines.php";
 
 // Functions/classes for data entry and processing
-require("inc_forms.php");
-require("inc_tables.php");
-require("inc_file_uploads.php");
+require_once "inc_forms.php";
+require_once "inc_tables.php";
+require_once "inc_file_uploads.php";
 
 // Journal System
-require("inc_journal.php");
+require_once "inc_journal.php";
 
 // Menus
-require("inc_menus.php");
+require_once "inc_menus.php";
 
 // Phone Home Functions
-require("inc_phone_home.php");
-
+require_once "inc_phone_home.php";
 
 log_debug("start", "Framework Load Complete.");
 
-
 /*
-	Load Application Configuration
+  Load Application Configuration
 
-	Some configuration is done locally (such as DB auth details), however most configuration is stored
-	inside the database to provide easier management, display and validation of configuration.
-*/
+  Some configuration is done locally (such as DB auth details), however most configuration is stored
+  inside the database to provide easier management, display and validation of configuration.
+ */
 
 log_debug("start", "Loading configuration from database");
 
-$sql_config_obj			= New sql_query;
-$sql_config_obj->string		= "SELECT name, value FROM config ORDER BY name";
+$sql_config_obj = New sql_query;
+$sql_config_obj->string = "SELECT name, value FROM config ORDER BY name";
 $sql_config_obj->execute();
 $sql_config_obj->fetch_array();
 
-foreach ($sql_config_obj->data as $data_config)
-{
-	if (!isset($GLOBALS["config"][ $data_config["name"] ]))
-	{
-		$GLOBALS["config"][ $data_config["name"] ] = $data_config["value"];
-	}
+foreach ($sql_config_obj->data as $data_config) {
+    if (!isset($GLOBALS["config"][$data_config["name"]])) {
+        $GLOBALS["config"][$data_config["name"]] = $data_config["value"];
+    }
 }
 
 unset($sql_config_obj);
 
-
-
 /*
-	Run Corrections
+  Run Corrections
 
-	Legacy adjustments to work around limitations that should be fixed
-	in future but can't be done at once without potentially breaking applications
-*/
+  Legacy adjustments to work around limitations that should be fixed
+  in future but can't be done at once without potentially breaking applications
+ */
 
 
 // if user debugging is set to disabled, make NULL so reports as empty()
-if (isset($_SESSION["user"]["debug"]))
-{
-	if ($_SESSION["user"]["debug"] == "disabled")
-	{
-		$_SESSION["user"]["debug"] = NULL;
-	}
+if (isset($_SESSION["user"]["debug"])) {
+    if ($_SESSION["user"]["debug"] == "disabled") {
+        $_SESSION["user"]["debug"] = NULL;
+    }
 }
 
-
-
 /*
-	Configure Local Timezone
+  Configure Local Timezone
 
-	Decent timezone handling was only implemented with PHP 5.2.0, so the ability to select the user's localtime zone
-	is limited to users running this software on PHPv5 servers.
+  Decent timezone handling was only implemented with PHP 5.2.0, so the ability to select the user's localtime zone
+  is limited to users running this software on PHPv5 servers.
 
-	Users of earlier versions will be limited to just using the localtime of the server - the effort required
-	to try and add timezone for older users (mainly PHPv4) is not worthwhile when everyone should be moving to PHP 5.2.0+
-	in the near future.
-*/
+  Users of earlier versions will be limited to just using the localtime of the server - the effort required
+  to try and add timezone for older users (mainly PHPv4) is not worthwhile when everyone should be moving to PHP 5.2.0+
+  in the near future.
+ */
 
-if (version_compare(PHP_VERSION, '5.2.0') === 1)
-{
-	log_debug("start", "Setting timezone based on user/system configuration");
-	
-	// fetch config option
-	if (isset($_SESSION["user"]["timezone"]))
-	{
-		// fetch from user preferences
-		$timezone = $_SESSION["user"]["timezone"];
-	}
-	else
-	{
-		// user hasn't chosen a default time format yet - use the system default
-		$timezone = sql_get_singlevalue("SELECT value FROM config WHERE name='TIMEZONE_DEFAULT' LIMIT 1");
-	}
+if (version_compare(PHP_VERSION, '5.2.0') === 1) {
+    log_debug("start", "Setting timezone based on user/system configuration");
 
-	// if set to SYSTEM just use the default of the server, otherwise
-	// we need to set the timezone here.
-	if ($timezone == "SYSTEM")
-	{
-		// set to the server default
-		log_debug("start", "Using server timezone default");
-		@date_default_timezone_set(@date_default_timezone_get());
-	}
-	else
-	{
-		// set to user selected or application default
-		log_debug("start", "Using application configured timezone");
+    // fetch config option
+    if (isset($_SESSION["user"]["timezone"])) {
+        // fetch from user preferences
+        $timezone = $_SESSION["user"]["timezone"];
+    } else {
+        // user hasn't chosen a default time format yet - use the system default
+        $timezone = sql_get_singlevalue("SELECT value FROM config WHERE name='TIMEZONE_DEFAULT' LIMIT 1");
+    }
 
-		if (!date_default_timezone_set($timezone))
-		{
-			log_write("error", "start", "A problem occured trying to set timezone to \"$timezone\"");
-		}
-		else
-		{
-			log_debug("start", "Timezone set to \"$timezone\" successfully");
-		}
-	}
+    // if set to SYSTEM just use the default of the server, otherwise
+    // we need to set the timezone here.
+    if ($timezone == "SYSTEM") {
+        // set to the server default
+        log_debug("start", "Using server timezone default");
+        date_default_timezone_set(date_default_timezone_get());
+    } else {
+        // set to user selected or application default
+        log_debug("start", "Using application configured timezone");
 
-	unset($timezone);
+        if (!date_default_timezone_set($timezone)) {
+            log_write("error", "start", "A problem occured trying to set timezone to \"$timezone\"");
+        } else {
+            log_debug("start", "Timezone set to \"$timezone\" successfully");
+        }
+    }
+
+    unset($timezone);
 }
 
-
-
 /*
-	Preload Language DB
+  Preload Language DB
 
-	The translation/errorloading options can be handled in one of two ways:
-	1. Preload all entries for the selected language (more memory, few SQL queries)
-	2. Only load translations as required (more SQL queries, less memory)
-*/
-
-
+  The translation/errorloading options can be handled in one of two ways:
+  1. Preload all entries for the selected language (more memory, few SQL queries)
+  2. Only load translations as required (more SQL queries, less memory)
+ */
 
 // ensure a language has been set in the user's profile, otherwise select
 // a default from the main configuration database
-if (!isset($_SESSION["user"]["lang"]))
-{
-	$_SESSION["user"]["lang"] = sql_get_singlevalue("SELECT value FROM config WHERE name='LANGUAGE_DEFAULT' LIMIT 1");
+if (!isset($_SESSION["user"]["lang"])) {
+    $_SESSION["user"]["lang"] = sql_get_singlevalue("SELECT value FROM config WHERE name='LANGUAGE_DEFAULT' LIMIT 1");
 }
-
 
 // determine whether or not to preload the language
-$language_mode			= sql_get_singlevalue("SELECT value FROM config WHERE name='LANGUAGE_LOAD' LIMIT 1");
-$GLOBALS["cache"]["lang_mode"]	= $language_mode;
+$language_mode = sql_get_singlevalue("SELECT value FROM config WHERE name='LANGUAGE_LOAD' LIMIT 1");
+$GLOBALS["cache"]["lang_mode"] = $language_mode;
 
-if ($language_mode == "preload")
-{
-	log_debug("start", "Preloading Language DB");
+if ($language_mode == "preload") {
+    log_debug("start", "Preloading Language DB");
 
+    // load all transactions
+    $sql_obj = New sql_query;
+    $sql_obj->string = "SELECT label, translation FROM `language` WHERE language='" . $_SESSION["user"]["lang"] . "'";
+    $sql_obj->execute();
+    $sql_obj->fetch_array();
 
-	// load all transactions
-	$sql_obj		= New sql_query;
-	$sql_obj->string	= "SELECT label, translation FROM `language` WHERE language='". $_SESSION["user"]["lang"] ."'";
-	$sql_obj->execute();
-	$sql_obj->fetch_array();
+    foreach ($sql_obj->data as $data) {
+        // add to cache
 
-	foreach ($sql_obj->data as $data)
-	{
-		// add to cache
-		$GLOBALS["cache"]["lang"][ $data["label"] ] = $data["translation"];
-	}
+        $GLOBALS["cache"]["lang"][$data["label"]] = $data["translation"];
+    }
 
-	log_debug("start", "Completed Language DB Preload");
+    log_debug("start", "Completed Language DB Preload");
 }
-
-
-
-?>
